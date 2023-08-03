541739b465d985d432ea6dcd373ed377034f3e42
TSQUARE-TSEGAYE <tsquare44tsegaye@gmail.com>
2023-08-03 05:18:55 -0700

    last committing

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,8 +1,15 @@
 #include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int de_v = 0;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
@@ -11,8 +18,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		de_v = 2 * de_v + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (de_v);
+	return (dec_val);
 }
